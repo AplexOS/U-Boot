7f8a3045e02fae582b6530848830585d998e7979
chenfulin5 <chenfulin5@gmail.com>
2016-11-30 16:26:24 +0800

    Add out git push

--- out/include/config.h
+++
@@ -1,8 +0,0 @@
-/* Automatically generated - do not edit */
-#define CONFIG_EMMC_BOOT	1
-#define CONFIG_BOARDDIR board/vscom/baltos
-#include <config_defaults.h>
-#include <config_uncmd_spl.h>
-#include <configs/baltos.h>
-#include <asm/config.h>
-#include <config_fallbacks.h>
