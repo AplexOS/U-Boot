c0fb85d18eb83d6683051833f1f7fe6a7750ac41
chenfulin5 <chenfulin5@gmail.com>
2016-12-02 09:56:09 +0800

    update mygitpush

--- mygitpush.sh
+++ mygitpush.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-
 TARGET=$1
+TARGET_BITBUCKET=git@bitbucket.org:chenfulin5/u-boot2016.05.git
+TARGET_GITLAB=git@192.168.1.4:chenfulin5/u-boot_2016.05.git
+
+
 
 if [ $# != 1 ]; then
     echo "Please input push target , bitbucket or gitlab or all "
@@ -9,15 +12,14 @@ if [ $# != 1 ]; then
 fi
 
 if [ "$TARGET" = "all" ]; then
-    git push git@bitbucket.org:chenfulin5/u-boot.git master
-    git push git@192.168.1.4:chenfulin5/u-boot_2016.05.git master
+    git push $TARGET_BITBUCKET master
+    git push $TARGET_GITLAB master
+elif [ "$TARGET" = "bitbucket" ]; then
+    git push $TARGET_BITBUCKET master
+elif [ "$TARGET" = "gitlab" ]; then
+    git push $TARGET_GITLAB master
+else
+    echo "Please input push target , bitbucket or gitlab or all "
+    exit 0
 fi
 
-
-if [ "$TARGET" = "bitbucket" ]; then
-    git push git@bitbucket.org:chenfulin5/u-boot.git master
-fi
-
-if [ "$TARGET" = "gitlab" ]; then
-    git push git@192.168.1.4:chenfulin5/u-boot_2016.05.git master
-fi
