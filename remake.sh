1cfff2728af26bd051c8c6002f083d9cdfb73eaa
chenfulin5 <chenfulin5@gmail.com>
2016-12-03 14:09:44 +0800

    Add copy u-boot image to CHEN_PATH

--- remake.sh
+++ remake.sh
@@ -3,3 +3,7 @@
 
 
 make -j2 O=out CROSS_COMPILE=arm-linux-gnueabihf- am335x_baltos_defconfig all
+
+cp /home/chen/am335x/u-boot_2016.05/out/MLO  $CHEN_PATH -rf
+cp /home/chen/am335x/u-boot_2016.05/out/u-boot.img  $CHEN_PATH  -rf
+echo  Copy u-boot image over ....
