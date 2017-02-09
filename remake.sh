13b780760323da9a97ecdc80e7ae1f5fc905e69b
tonyliu00 <tonyliu00@163.com>
2017-02-09 10:38:00 +0800

    first commit

---
+++ remake.sh
@@ -0,0 +1,9 @@
+make clean
+
+make mx6dl_sabresd_android_config
+
+make -j2
+
+cp u-boot.bin /mnt/hgfs/Desktop/CM-SOMB-IMX6_Q4.8.5_TOOL/Profiles/MX6DL\ Linux\ Update/OS\ Firmware/files/
+
+echo "work over.    :)"
